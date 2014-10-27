df5ebf113d693d4230be7a334a2dc6d076587153
Victor Hugo Marques <vicothm@outlook.com>
2014-10-27 16:54:58 -0200

    first commit

---
+++ AutoMapperExtensions/AutoMapperExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace AutoMapperExtensions
+{
+    public static class AutoMapperExtensions
+    {
+        public static List<TResult> MapTo<TResult>(this IEnumerable self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+
+            return (List<TResult>)Mapper.Map(self, self.GetType(), typeof(List<TResult>));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self"></param>
+        /// <returns></returns>
+        public static TResult MapTo<TResult>(this object self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+
+            return (TResult)Mapper.Map(self, self.GetType(), typeof(TResult));
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="self">source</param>
+        /// <param name="value">destination</param>
+        /// <returns></returns>
+        public static TResult MapPropertiesToInstance<TResult>(this object self, TResult value)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+
+            return (TResult)Mapper.Map(self, value, self.GetType(), typeof(TResult));
+        }
+
+        public static TResult DynamicMapTo<TResult>(this object self)
+        {
+            if (self == null)
+                throw new ArgumentNullException();
+
+            return (TResult)Mapper.DynamicMap(self, self.GetType(), typeof(TResult));
+        }
+    }
+}
